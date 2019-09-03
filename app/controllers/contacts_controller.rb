6cb2db1eb3d97edc858479c268ce666f8e934b38
Juyeong Lee <2jy22@naver.com>
2019-09-04 00:15:00 +0900

    2-4 : Contact 작성 기능 구현

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   def index
+    @all_contacts = Contact.all # 배열 형태로 담기게 된다.
   end
 
   def new
@@ -9,5 +10,14 @@ class ContactsController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @content = params[:content]
+
+    new_contact = Contact.new(name: @name, email: @email, content: @content)
+    if new_contact.save
+      # save가 됐을 때 데이터베이스에 완전히 제대로 저장이 된다면 리턴값은 true,
+      # save하는 과정에 문제가 생기게 되어서 실패하면 false를 리턴
+      redirect_to "/contacts/index"
+    else
+      redirect_to "/contacts/new"
+    end
   end
 end
