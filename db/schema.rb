b593a13a6c210ab97e3fc258a5748a22e62e92dc
Juyeong Lee <2jy22@naver.com>
2019-09-04 13:06:28 +0900

    3-1 : User model 만들기

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190903143108) do
+ActiveRecord::Schema.define(version: 20190904040009) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "email"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20190903143108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
