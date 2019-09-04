274c9cbee2c7957532984111edaacb844ac530bd
Juyeong Lee <2jy22@naver.com>
2019-09-04 14:13:34 +0900

    3-4 : 헬퍼를 이해하고 로그아웃 기능 구현

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper # 컨트롤러에서 헬퍼 사용하기 위함
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email]) # 대소문자 구분 없이 : .downcase
     if user && user.authenticate(params[:password]) # text로 입력한 password를 해쉬값으로 바꿈
-      session[:user_id] = user.id
+      # session[:user_id] = user.id
+      log_in(user)
       redirect_to '/'
     else
       flash[:alert] = 'Invalid email/password combination'# flash에 메세지를 넣게 되면 다음 액션에서 호출해서 사용 가능
       redirect_to new_session_path
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to '/'
   end
 end
